1241ffc6a74b1930735cce1c01215cbe16e16c59
TaFhiM12 <tafhim0000001@gmail.com>
2024-11-24 22:52:36 +0600

    11/24/24

---
+++ sample.js
@@ -0,0 +1,8 @@
+
+function test(){
+    document.getElementById("test").style.backgroundColor("black")
+    alert("test function")
+
+    const fn = parseInt(document.getElementById("test").value);
+
+}
